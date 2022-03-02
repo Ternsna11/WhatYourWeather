652d692f9e0c8b24ef9d091000fc8185a0407993
ternsna11 <ternsna11@gmail.com>
2022-03-02 17:12:51 -0600

    backend.....oh yeah, baby....backend

--- src/api.js
+++ src/api.js
@@ -3,3 +3,18 @@ const API =
 
 
   // enter post request here
+
+
+// Post request
+// const postRequest = fetch(API, {
+//   method: 'POST',
+//   body: JSON.stringify({
+//     zipcode: zipcode,
+//     data: body.data
+//   })
+// })
+// .then(response => response.json())
+// .then(data => {
+//   console.log(data);
+// })
+// .catch(error => console.log(error));
